97fb9a3d290da85a814f3c604b50e66021add763
atbuy <buy@atbuy.dev>
2024-10-19 19:42:53 +0300

    Added initial data

---
+++ db/2-insert-data.sql
@@ -0,0 +1,26 @@
+INSERT INTO KATHGORIA(name, description) VALUES ("GYMNASIO", "Gymnasio");
+INSERT INTO KATHGORIA(name, description) VALUES ("LYKEIO", "Lykeio");
+INSERT INTO KATHGORIA(name, description) VALUES ("EPAL", "Epal");
+INSERT INTO KATHGORIA(name, description) VALUES ("MOYSIKO", "Moysiko");
+INSERT INTO KATHGORIA(name, description) VALUES ("EIDIKO", "Eidiko");
+
+INSERT INTO SXOLEIO(code, name, address, category) VALUES ('A1234', 'School 1', 'Address 1', 'GYMNASIO');
+INSERT INTO SXOLEIO(code, name, address, category) VALUES ('B1234', 'School 2', 'Address 2', 'LYKEIO');
+INSERT INTO SXOLEIO(code, name, address, category) VALUES ('C1234', 'School 3', 'Address 3', 'LYKEIO');
+INSERT INTO SXOLEIO(code, name, address, category) VALUES ('D1234', 'School 4', 'Address 4', 'LYKEIO');
+INSERT INTO SXOLEIO(code, name, address, category) VALUES ('E1234', 'School 5', 'Address 5', 'EPAL');
+INSERT INTO SXOLEIO(code, name, address, category) VALUES ('F1234', 'School 6', 'Address 6', 'MOYSIKO');
+INSERT INTO SXOLEIO(code, name, address, category) VALUES ('G1234', 'School 7', 'Address 7', 'EIDIKO');
+
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 1', 1, 'A1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 2', 3, 'A1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 3', 2, 'B1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 4', 4, 'C1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 5', 5, 'D1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 6', 20, 'A1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 7', 5, 'E1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 8', 10, 'F1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 9', 9, 'G1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 10', 7, 'E1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 11', 3, 'B1234');
+INSERT INTO EKPAIDEYTIKOS(full_name, work_years, school) VALUES ('Kathigitis 12', 4, 'B1234');
