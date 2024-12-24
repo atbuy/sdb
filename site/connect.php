14dd6f7f4dc4b75ac875db443fc84f662a80e283
atbuy <buy@atbuy.dev>
2024-12-24 15:41:57 +0200

    Changed to spaces instead of tabs

--- site/connect.php
+++ site/connect.php
@@ -1,11 +1,9 @@
-
 <?php 
-
   // Edit before local usage
-	$host = 'localhost';
-	$user = '';
-	$pass = '';
-	$name = '';
+  $host = "localhost";
+  $user = "";
+  $pass = "";
+  $name = "";
 	
 	$conn = mysqli_connect($host, $user, $pass, $name) or die("Connection failed");
 ?>
