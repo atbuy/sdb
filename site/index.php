a4758e6b85366163d6d7ad8c9f40ac7cd0b036f9
atbuy <buy@atbuy.dev>
2024-12-24 22:06:59 +0200

    Added favicon

--- site/index.php
+++ site/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="icon" href="static/images/icon.png" />
+
     <link rel="stylesheet" href="static/css/main.css" />
 
     <!-- Add tailwind CDN for styles -->
