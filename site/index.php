8f8b75f7320f9a4e3cd6bf7746a92903880235f3
atbuy <buy@atbuy.dev>
2024-12-29 22:09:37 +0200

    Added toasts in index, allow toggling between editing for each row

--- site/index.php
+++ site/index.php
@@ -2,6 +2,8 @@
   include "./mathima/select.php";
   include "./components/icons/edit.php";
   include "./components/icons/delete.php";
+  include "./components/icons/save.php";
+  include "./components/icons/cancel.php";
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +18,9 @@
 
     <!-- Add tailwind CDN for styles -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Add Toastify styles for toast type notifications -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 
     <title>Team 19 - DB</title>
 
@@ -33,7 +38,7 @@
       </div>
 
       <!-- "MATHIMA" table -->
-      <table class="bg-zinc-900 rounded-lg table-auto text-left">
+      <table class="bg-zinc-900 rounded-lg table-auto text-left shadow-lg">
         <thead>
           <tr class="bg-zinc-950">
             <th class="px-3 py-4 rounded-tl-lg">ID</th>
@@ -45,14 +50,35 @@
         </thead>
         <tbody x-data='{ rows: <?php echo $MATHIMA_DATA ?> }'>
           <template x-for="row in rows" :key="row.id">
-            <tr>
+            <tr :data-mathima-row="row.id" x-data="{ editing: false, toggleEdit() { this.editing = !this.editing } }">
               <td class="px-3 py-4 text-rose-600 font-semibold" x-text="row.id"></td>
-              <td class="px-3 py-4 text-green-400" x-text="row.name"></td>
-              <td class="px-3 py-4 text-rose-600 font-semibold" x-text="row.active_year"></td>
+              <td class="px-3 py-4 text-green-400">
+                <input
+                  class="border-b-2 border-white bg-transparent"
+                  :value="row.name"
+                  x-show="editing"
+                  :data-mathima-input="row.id"
+                  data-mathima-input-key="name"
+                />
+                <span x-show="!editing" x-text="row.name"></span>
+              </td>
+              <td class="px-3 py-4 text-rose-600 font-semibold">
+                <input
+                  class="border-b-2 border-white bg-transparent"
+                  type="number"
+                  :value="row.active_year"
+                  x-show="editing"
+                  min="0"
+                  max="99"
+                  :data-mathima-input="row.id"
+                  data-mathima-input-key="active_year"
+                />
+                <span x-show="!editing" x-text="row.active_year"></span>
+              </td>
               <td class="px-3 py-4 text-blue-400" x-text="row.school_category"></td>
-              <td class="px-3 py-4 flex justify-around items-center">
+              <td class="px-3 py-4 flex justify-around items-center" x-show="!editing">
                 <div>
-                  <button class="action-button update" onclick="editMathima(this)" :data-rowID="row.id">
+                  <button class="action-button update" x-on:click="toggleEdit()">
                     <?php echo $EDIT_ICON ?>
                   </button>
                 </div>
@@ -62,15 +88,34 @@
                   </button>
                 </div>
               </td>
+              <td class="px-3 py-4 flex justify-around items-center" x-show="editing">
+                <div>
+                  <button
+                    class="action-button save"
+                    onclick="editMathimaSave(this)"
+                    :data-rowID="row.id"
+                  >
+                    <?php echo $SAVE_ICON ?>
+                  </button>
+                </div>
+                <div>
+                  <button class="action-button cancel" x-on:click="toggleEdit()">
+                    <?php echo $CANCEL_ICON ?>
+                  </button>
+                </div>
+              </td>
+
             </tr>
           </template>
-          
-          <!-- Load all rows from the "Mathima" table -->
         </tbody>
       </table>
     </div>
     
     <script src="static/js/main.js"></script>
+    <script src="static/js/toasts.js"></script>
+
+    <!-- Toastify notifications -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 
   </body>
 </html>
