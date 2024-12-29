19f9826494d621c1045e130561d18733862ccd3d
atbuy <buy@atbuy.dev>
2024-12-29 22:10:10 +0200

    Added functionality for save and delete buttons

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -1,9 +1,74 @@
-const editMathima = (elem) => {
-  console.log("update", elem);
+const editMathimaSave = (elem) => {
+  const rowID = parseInt(elem.dataset.rowid);
+  const inputs = document.querySelectorAll(`[data-mathima-input="${rowID}"]`);
+
+  let data = { id: rowID };
+
+  inputs.forEach((input) => {
+    const key = input.dataset.mathimaInputKey;
+    data[key] = input.value;
+  });
+
+  const payload = JSON.stringify(data);
+
+  // Start new AJAX request
+  const request = new XMLHttpRequest();
+
+  // Handle cases to update row depending on response status
+  request.onreadystatechange = () => {
+    if (request.readyState !== XMLHttpRequest.DONE) return;
+
+    if (request.status === 200) {
+      toastSuccess("Data updated successfully.");
+      return;
+    }
+
+    if (request.status === 400) {
+      toastError("Invalid data sent to server.");
+      return;
+    }
+
+    toastError("Something went wrong.");
+  };
+
+  // Set options and send request to update.php
+  request.open("PATCH", "/mathima/update.php", true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(payload);
 };
 
 const deleteMathima = (elem) => {
-  console.log("delete", elem)
+  const rowID = parseInt(elem.dataset.rowid);
+  const data = { id: rowID };
+  const payload = JSON.stringify(data);
+
+  // Start new AJAX request
+  const request = new XMLHttpRequest();
+
+  // Handle cases to update row depending on response status
+  request.onreadystatechange = () => {
+    if (request.readyState !== XMLHttpRequest.DONE) return;
+
+    if (request.status === 200) {
+      toastSuccess("Row deleted successfully.");
+
+      // Response was successful so we need to delete the row clicked
+      const row = document.querySelector(`tr[data-mathima-row="${rowID}"]`);
+      row.remove();
+
+      return;
+    }
+
+    if (request.status === 400) {
+      toastError("Invalid data sent to server.");
+      return;
+    }
+
+    toastError("Something went wrong.");
+  };
+
+  // Set options and send request to update.php
+  request.open("DELETE", "/mathima/delete.php", true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(payload);
 };
-
-
