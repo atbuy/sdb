1f9dd4657bc83ca4f6bcf74da50099f4cda9b3e7
atbuy <buy@atbuy.dev>
2024-12-31 11:32:54 +0200

    Added table icon

--- site/index.php
+++ site/index.php
@@ -1,12 +1,16 @@
+<?php
+  include './components/icons/table.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" href="static/images/icon.png" />
+    <link rel="icon" href="/static/images/icon.png" />
 
-    <link rel="stylesheet" href="static/css/main.css" />
+    <link rel="stylesheet" href="/static/css/main.css" />
 
     <!-- Add tailwind CDN for styles -->
     <script src="https://cdn.tailwindcss.com"></script>
@@ -14,7 +18,7 @@
     <!-- Add Toastify styles for toast type notifications -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 
-    <title>Team 19 - DB</title>
+    <title>Team 19 - DB2</title>
 
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
   </head>
@@ -26,7 +30,10 @@
     </div>
 
     <div class="py-5 flex justify-center items-center">
-      <a href="/mathima" class="px-6 py-4 mx-6 shadow-lg rounded-lg bg-zinc-900"><b>MATHIMA</b></a>
+        <a href="/mathima" class="flex justify-center items-center px-6 py-4 mx-6 shadow-lg rounded-lg bg-zinc-900 hover:scale-110 transition-transform">
+          <span class="mr-3 fill-white"><?php echo $TABLE_ICON ?></span>
+          <span class="mx-2 font-bold">MATHIMA</span>
+        </a>
     </div>
 
   </body>
